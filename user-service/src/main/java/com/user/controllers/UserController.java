29031a8334080f9a2480fc15e05ceb322e2f76cc
Rincy CP <Rincy CP@192.168.1.37>
2025-04-19 16:02:11 +0530

    feat(user-service) : Implemented Controller, Entity, Repository and application properties.

---
+++ user-service/src/main/java/com/user/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.user.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.user.entities.User;
+import com.user.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserRepository userRepository;
+
+    @PostMapping
+    public User save(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
+
